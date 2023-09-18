b6d54bded93b282f0bd3f1b15a2355f62d799d34
Carrie Kraft <carriekarft@gmail.com>
2023-09-17 20:55:52 -0700

    added validation for unique username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :phone, length: {is: 10}
     has_secure_password
+
 end
