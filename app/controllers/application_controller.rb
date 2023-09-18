5466482c58b97915c1bf27a2cbbc8a7fc80b33f5
Carrie Kraft <carriekarft@gmail.com>
2023-09-18 14:29:02 -0700

    added some authorization logic to be passed down to other conrollers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  before_action :authorize
+
+  private
+
+  def authorize
+    return render json: {error: "You need to be logged in to access this"}, status: 422 unless session.include? :user_id
+  end
+
 end
