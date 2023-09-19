7fbf5fcdb50fd30806f5ceba42fc2cca19a7f8dc
Carrie Kraft <carriekarft@gmail.com>
2023-09-18 21:42:41 -0700

    adding exception and error handleing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid
   include ActionController::Cookies
 
   before_action :authorize
@@ -7,6 +8,10 @@ class ApplicationController < ActionController::API
 
   def authorize
     return render json: {error: "You need to be logged in to access this"}, status: 422 unless session.include? :user_id
+  end
+
+  def invalid(exception)
+    return render json: {errors: exception.record.errors.full_messages}, status: 422
   end
 
 end
