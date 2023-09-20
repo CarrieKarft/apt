66ae25c64f422415aa9ecc3b795904a1dbef2ffe
Carrie Kraft <carriekarft@gmail.com>
2023-09-19 18:24:35 -0700

    updated login method to authenticate password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,19 @@ class SessionsController < ApplicationController
   skip_before_action :authorize, only: [:create]
   # logs user in
   def create
+    # works
     user = User.find_by(username: params[:username])
-    session[:user_id] = user.id
-    render json: user
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: user
+    else
+      render json: {error: "Wrong username or password"}, status: 422
+    end
   end
 # logs user out by deleting the user_id from the session hash
   def destroy
-    session.destroy :user_id
-    render head :no_content
+    # works
+    session.delete :user_id
+    head :no_content
   end
 end
