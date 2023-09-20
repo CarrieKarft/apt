2d2938f2950fd643a528ee2a9aa06eec4ef3d9ef
Carrie Kraft <carriekarft@gmail.com>
2023-09-19 18:28:15 -0700

    added routes for applications model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :applications
   resources :sessions, only: [:create, :destroy]
   resources :users
   # Routing logic: fallback requests for React Router.
