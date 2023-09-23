b55b211acc8cb9462b3b6312ddc0958a9e8f7ee0
Carrie Kraft <carriekarft@gmail.com>
2023-09-23 14:32:40 -0700

    added show index and create routes for appartments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   
-  resources :applications
+  resources :appartments, only: [:index, :show, :create]
+  resources :applications, except: [:index]
   resources :sessions, only: [:create, :destroy]
-  resources :users
+  resources :users, except: [:index]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
