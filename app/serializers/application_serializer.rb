824f2e12d01cb4687f6cb551dee06c94c2d0384b
Carrie Kraft <carriekarft@gmail.com>
2023-09-23 14:30:38 -0700

    got rid of association macro for user since app doesnt need applications to contain nested user json data

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -1,3 +1,4 @@
 class ApplicationSerializer < ActiveModel::Serializer
   attributes :id, :appartment_id, :user_id, :applicant_name, :current_residence_phone, :current_residence_address, :yearly_income, :applicant_email
+  # belongs_to :user
 end
