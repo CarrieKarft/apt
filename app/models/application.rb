ab46a60c75ca42a91ec55f8cad3f97290d8bd87e
Carrie Kraft <carriekarft@gmail.com>
2023-09-23 14:28:16 -0700

    added association macros

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,12 @@
 class Application < ApplicationRecord
+    validates :appartment_id, presence: :true
+    # clean up validations
+    validates :applicant_name, presence: :true
+    validates :applicant_email, presence: true
+    validates :current_residence_phone, presence: :true, length: {is: 10}
+    validates :current_residence_address, presence: :true
+
     belongs_to :user
+    belongs_to :appartments
 end
+
