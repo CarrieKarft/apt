614f99a4d813d6a7b7516d4689c1c5e9e120b0ad
Carrie Kraft <carriekarft@gmail.com>
2023-09-23 14:31:56 -0700

    added association macro to have user data include nested application data

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :username, :phone, :location, :password_digest
+  # need to display appartments info not applications
+  has_many :applications
 end
