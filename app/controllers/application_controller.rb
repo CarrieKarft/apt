cbf2d0aeacc64752092ae9626383a6fa7e7f9223
Carrie Kraft <carriekarft@gmail.com>
2023-09-23 14:25:06 -0700

    created find_user method to eliminate some repeated code throughout controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid, with: :invalid
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
   include ActionController::Cookies
 
   before_action :authorize
 
   private
+  def find_user
+    User.find(session[:user_id])
+  end
+
+  def render_not_found
+    render json: {error: "Item not found"}, status: :not_found
+  end
 
   def authorize
     return render json: {error: "You need to be logged in to access this"}, status: 422 unless session.include? :user_id
