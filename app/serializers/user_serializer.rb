5cec34dec9e7b6142c7e6485e1db422767065746
Carrie Kraft <carriekarft@gmail.com>
2023-09-23 21:18:50 -0700

    removed password_digest and added additional association macro

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :username, :phone, :location, :password_digest
+  # took out password_digest and added an additional has_many
+  attributes :id, :name, :username, :phone, :location
   # need to display appartments info not applications
   has_many :applications
+  has_many :appartments
 end
