de270837cc1110a385418960eb67053b542df8e8
Carrie Kraft <carriekarft@gmail.com>
2023-09-28 18:40:29 -0700

    added child component tags to add

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import AppartmentsListingPage from './components/ApartmentsListingPage';
+import SignupPage from './components/SignupPage';
+import LoggedInUserPage from './components/LoggedInUserPage';
+import NewApplicationForm from './components/NewApplicationForm';
+import CompletedAppliction from './components/CompletedApplication';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <AppartmentsListingPage /> */}
+      {/* <CompletedAppliction /> */}
+      {/* <NewApplicationForm /> */}
+      {/* <SignupPage /> */}
+      {/* <LoggedInUserPage /> */}
     </div>
   );
 }
