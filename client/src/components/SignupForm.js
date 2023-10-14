7be2a700c203af1d7539a8ac88216e187bf6e666
Carrie Kraft <carriekarft@gmail.com>
2023-10-14 13:18:55 -0700

    made form into controlled form and added onSubmit that invokes function to sign user up, function located in parent component

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,33 +1,64 @@
+import {useState} from 'react'
 
-
-function SignupForm() {
+function SignupForm({ handleSignupUser }) {
     // fetch will be to user#create
+    const [name, setName] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [password_confirmation, setPasswordConfirmation] = useState('')
+    const [phone, setPhone] = useState('')
+    const [location, setLocation] = useState('')
+    function handleOnSubmit(e) {
+        e.preventDefault()
+        const newUserObj = {
+            name,
+            password,
+            password_confirmation,
+            username,
+            phone,
+            location
+        }
+        handleSignupUser(newUserObj)
+    }
+
     return (
         <div>
             <h2 style={{color: "red"}}>SignupForm</h2>
-            <form>
+            <form onSubmit={e => handleOnSubmit(e)}>
                 <lable> Full Name: 
                     <input type="text"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
                     ></input>
                 </lable>
                 <lable> Username: 
                     <input type="text"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
                     ></input>
                 </lable>
                 <lable> Password: 
                     <input type="text"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
                     ></input>
                 </lable>
                 <lable> Password Confirmation: 
                     <input type="text"
+                    value={password_confirmation}
+                    onChange={e => setPasswordConfirmation(e.target.value)}
                     ></input>
                 </lable>
                 <lable> Phone Number: 
                     <input type="text"
+                    value={phone}
+                    onChange={e => setPhone(e.target.value)}
                     ></input>
                 </lable>
                 <lable> Location: 
                     <input type="text"
+                    value={location}
+                    onChange={e => setLocation(e.target.value)}
                     ></input>
                 </lable>
                 <input type="submit"></input>
