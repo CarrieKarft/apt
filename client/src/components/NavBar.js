1012bde03e9168bbf07ae092ff417fb201e8f8e7
Carrie Kraft <carriekarft@gmail.com>
2023-10-14 13:17:09 -0700

    added onClick event handller to envoke function responsible for logging user out, function is located in parent component

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,13 +1,13 @@
 
 
-function NavBar() {
+function NavBar({ onHandleLogout }) {
     return (
         // might get rid of view applicaitons
         // all of these will become links with reactRouterDom
         // search will take you to AppartmetsListingPage
         // Logout will trigger fetch to sessions#delete
         <div>
-            <button>Logout</button>
+            <button onClick={(e) => onHandleLogout(e.target.value)}>Logout</button>
             <button>Search For Appartments</button>
             <button>View Applications</button>
         </div>
