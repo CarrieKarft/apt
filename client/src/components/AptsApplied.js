ffe8e0821a95c15f40d37b5937f385fd106e1260
Carrie Kraft <carriekarft@gmail.com>
2023-12-17 19:53:30 -0800

    changed apartments to be unique_apartments custome serializer method return to avoid showing duplicates of apartments

--- client/src/components/AptsApplied.js
+++ client/src/components/AptsApplied.js
@@ -5,22 +5,22 @@ import { UserContext } from "../context/UserContext";
 
 function AptsAppliedTo() {
     const {currentUser} = useContext(UserContext)
-    const {apartments} = currentUser
+    const {unique_apartments} = currentUser
     if(!currentUser) return <h2>Loading...</h2>
-    if (apartments.length < 1) return <p>Looks like you haven't applied to any apartments yet. Click here to view new listings</p>
+    if (unique_apartments.length < 1) return <p>Looks like you haven't applied to any apartments yet. Click here to view new listings</p>
     // console.log("currentUser", currentUser)
-    const mappingUserApartments = currentUser.apartments.map((apartment, index) => {
+    const mappingUserApartments = currentUser.unique_apartments.map((apartment, index) => {
         return (
             <div key={index}>
                 <Apartment apartment={apartment} />
-                <NavLink to={`/appartment/${apartment.id}/application`}>click here to view your completed application for {apartment.name}</NavLink>
+                <NavLink to={`/appartment/${apartment.id}/application`}>click here to view your completed applications for {apartment.name}</NavLink>
             </div>
         )
     })
     return (
         <div>
             <h2 style={{color: "red"}}>AptsAppliedTo</h2>
-            {/* click on appartment to view applicaiton? */}
+            {/* click on appartment to view application? */}
             {mappingUserApartments}
         </div>
     )
