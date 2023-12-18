eeffaf56127306d991f1e0ef0f44888a7d65525c
Carrie Kraft <carriekarft@gmail.com>
2023-12-17 19:46:56 -0800

    creating custom method to return an array of unique apartments only for user

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,18 @@
 class UserSerializer < ActiveModel::Serializer
   # took out password_digest and added an additional has_many
-  attributes :id, :name, :username, :phone, :location
+  attributes :id, :name, :username, :phone, :location, :unique_apartments, :first_name
   # need to display appartments info not applications
   has_many :applications
   has_many :apartments
+
+  def unique_apartments
+    apartments = object.apartments
+    # byebug
+    apartments.uniq {|apt| apt.id}
+  end
+# come back to this when you are working on finishing touches
+  def first_name
+    name = object.name.split(" ")
+    # byebug
+  end
 end
