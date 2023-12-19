92cef60a4bcc871dc83989261f8ea3e782c881d9
Carrie Kraft <carriekarft@gmail.com>
2023-12-19 14:08:42 -0800

    updated route to loggged in user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  
-  resources :appartments, only: [:index, :show, :create]
-  resources :applications, except: [:index]
-  resources :sessions, only: [:create, :destroy]
-  resources :users, except: [:index]
+    resources :apartments, only: [:index, :show, :create]
+    resources :applications
+    resources :sessions, only: [:create, :destroy]
+    resources :users, except: [:index, :show]
+    get '/me', to: "users#show"
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
