3d5752d8c04a9f0004782aadbf6e862154cbd2be
Carrie Kraft <carriekarft@gmail.com>
2023-12-21 19:35:00 -0800

    removed unneeded comments

--- app/serializers/apartment_serializer.rb
+++ app/serializers/apartment_serializer.rb
@@ -1,7 +1,6 @@
 class ApartmentSerializer < ActiveModel::Serializer
   attributes :id, :name, :number_of_rooms, :parking_available, :monthly_rent, :address, :phone, :pets_allowed, :available_to_rent
-  # maybe create custome serializer method to show #of applications only but no specific application info
-  # has_many :users
+
   has_many :applications
 
 end
