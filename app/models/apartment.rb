e2e4e124b21e35ed6d5ca0b9e2c5c4d185ff1058
Carrie Kraft <carriekarft@gmail.com>
2023-12-21 19:29:15 -0800

    fixed phone length vlidation to be a minimum instead of a difinitive length

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -4,16 +4,6 @@ class Apartment < ApplicationRecord
 
     validates :name, :number_of_rooms, :address, :phone, presence: true
     validates :available_to_rent, :parking_available, presence: true, allow_blank: true
-    # change is to min probably
-    validates :phone, length: {is: 10}
+    validates :phone, length: {minimum: 10}
 
 end
-
-# t.string "name"
-# t.integer "number_of_rooms"
-# t.boolean "parking_available"
-# t.integer "monthly_rent"
-# t.string "address"
-# t.string "phone"
-# t.string "pets_allowed"
-# t.boolean "available_to_rent"
