2a7dcd4b1331d70cdf29e8e87a9b861e4c68d3df
Carrie Kraft <carriekarft@gmail.com>
2023-12-21 19:36:22 -0800

    removed unneeded commen ts

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,19 +1,19 @@
 class UserSerializer < ActiveModel::Serializer
   # took out password_digest and added an additional has_many
-  attributes :id, :name, :username, :phone, :location, :unique_apartments, :first_name
+  attributes :id, :name, :username, :phone, :location, :first_name, :unique_apartments
   # need to display appartments info not applications
   has_many :applications
-  # has_many :apartments
+  has_many :apartments
 
   def unique_apartments
     apartments = object.apartments
     # byebug
     apartments.uniq {|apt| apt.id}
   end
-# come back to this when you are working on finishing touches
+
   def first_name
     name = object.name.split(" ")
-    name.shift
+    name.first
     # byebug
   end
 end
