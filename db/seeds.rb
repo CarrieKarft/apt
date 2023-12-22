45156c0aacc9bbed4b03bc4552a5eb19964945d6
Carrie Kraft <carriekarft@gmail.com>
2023-12-21 20:10:24 -0800

    updated seeds file to create data for applications

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,17 +23,25 @@
 #     t.string "pets_allowed"
 #     t.boolean "available_to_rent"
 puts "seeding..."
-    Appartment.create(name: "Parkrose Apartments", number_of_rooms: 2, parking_available: true, monthly_rent: 2500, address: "4444 SE Parkrose Ave, New York, NY 888888", phone: "5555555455", pets_allowed: "cats are allowed", available_to_rent: true)
+    User.create(name: "Carrie Kraft", username: "ckraft", phone: '5555555555', location: "Portland, OR", password: "ckraft", password_confirmation: "ckraft")
 
-    Appartment.create(name: "Regal Apartments", number_of_rooms: 4, parking_available: true, monthly_rent: 1900, address: "777 NW Regal Ave, Atlanta, GA 999999", phone: "8885445444", pets_allowed: "Pets are allowed under 70lbs", available_to_rent: true)
+    User.create(name: "Eric Kraft", username: "ekraft", phone: '6666666666', location: "Portland, OR", password: "ekraft", password_confirmation: "ekraft")
 
-    Appartment.create(name: "Forest Apartments", number_of_rooms: 1, parking_available: false, monthly_rent: 2300, address: "777 SW Street Ave, Las Angelos, CA 000999", phone: "4332445444", pets_allowed: "No pets are allowed", available_to_rent: true)
+    User.create(name: "Dougie", username: "dougie22", phone: '7777777777', location: "Portland, OR", password: "dougie22", password_confirmation: "dougie22")
 
-    Appartment.create(name: "Sunrise Villa", number_of_rooms: 4, parking_available: true, monthly_rent: 4000, address: "6666 NE Hollywood Ave, Las Angelos, CA 000998", phone: "4332447890", pets_allowed: "No pets are allowed", available_to_rent: true)
+    User.create(name: "Damien", username: "damien33", phone: '8888888888', location: "Portland, OR", password: "damien33", password_confirmation: "damien33")
+
+    Apartment.create(name: "Parkrose Apartments", number_of_rooms: 2, parking_available: true, monthly_rent: 2500, address: "4444 SE Parkrose Ave, New York, NY 888888", phone: "5555555455", pets_allowed: "cats are allowed", available_to_rent: true)
+
+    Apartment.create(name: "Regal Apartments", number_of_rooms: 4, parking_available: true, monthly_rent: 1900, address: "777 NW Regal Ave, Atlanta, GA 999999", phone: "8885445444", pets_allowed: "Pets are allowed under 70lbs", available_to_rent: true)
+
+    Apartment.create(name: "Forest Apartments", number_of_rooms: 1, parking_available: false, monthly_rent: 2300, address: "777 SW Street Ave, Las Angelos, CA 000999", phone: "4332445444", pets_allowed: "No pets are allowed", available_to_rent: true)
+
+    Apartment.create(name: "Sunrise Villa", number_of_rooms: 4, parking_available: true, monthly_rent: 4000, address: "6666 NE Hollywood Ave, Las Angelos, CA 000998", phone: "4332447890", pets_allowed: "No pets are allowed", available_to_rent: true)
 
     User.all.each do |user|
         rand(1..4).times do
-            user.applications.create(appartment_id: rand(1..3), applicant_name: Faker::Name.name, current_residence_phone: "9999999999", applicant_email: Faker::Internet.email, current_residence_address: Faker::Address.full_address, yearly_income: "$100,000")
+            user.applications.create(apartment_id: rand(1..3), applicant_name: Faker::Name.name, current_residence_phone: "9999999999", applicant_email: Faker::Internet.email, current_residence_address: Faker::Address.full_address, yearly_income: "$100,000")
         end
     end
 puts "done seeding!"
