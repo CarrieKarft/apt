404ef1c83d843fffdb2482ce5096afad4d4234ef
Carrie Kraft <carriekarft@gmail.com>
2023-12-21 19:51:02 -0800

    added class names for styling

--- client/src/components/AptsApplied.js
+++ client/src/components/AptsApplied.js
@@ -1,17 +1,15 @@
 import React, {useContext} from "react";
-import { useParams, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Apartment from "./Apartment";
 import { UserContext } from "../context/UserContext";
 
 function AptsAppliedTo() {
     const {currentUser} = useContext(UserContext)
     const {unique_apartments} = currentUser
-    if(!currentUser) return <h2>Loading...</h2>
-    if (unique_apartments.length < 1) return <p>Looks like you haven't applied to any apartments yet. Click here to view new listings</p>
-    // console.log("currentUser", currentUser)
+    if (unique_apartments.length < 1) return <p style={{color: "white"}}>Looks like you haven't applied to any apartments yet. Select 'Search For Apartments' Above To View New Listings</p>
     const mappingUserApartments = currentUser.unique_apartments.map((apartment, index) => {
         return (
-            <div key={index}>
+            <div key={index} className='mappedDiv'>
                 <Apartment apartment={apartment} />
                 <NavLink to={`/appartment/${apartment.id}/application`}>click here to view your completed applications for {apartment.name}</NavLink>
             </div>
@@ -19,8 +17,7 @@ function AptsAppliedTo() {
     })
     return (
         <div>
-            <h2 style={{color: "red"}}>AptsAppliedTo</h2>
-            {/* click on appartment to view application? */}
+            <h2 style={{color: "white"}}>Apartments You've Applied To</h2>
             {mappingUserApartments}
         </div>
     )
