6d2c9bfdc7ea933a9e97e7455a094e74c601dc87
Carrie Kraft <carriekarft@gmail.com>
2023-12-21 19:50:08 -0800

    added class names for styling, and some conditional rendering logic to display the update form when the update button is clicked

--- client/src/components/Application.js
+++ client/src/components/Application.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import UpdateApplicationForm from "./UpdateApplicaitonForm";
 
 function Appliction({app, id, handleApplicationDelete, handleUpdatingUserApplication }) {
+    const [isClicked, setIsClicked] = useState(false)
+    console.log(app)
     const {apartment_id} = app
     return (
-        <div>
-        {/* How will I get appartment name? */}
+        <div className='mappedDiv'>
         <h3>Applicant Name: {app.applicant_name}</h3>
         <h4>Applicant Email: {app.applicant_email}</h4>
-        <h4>Applicant Phone: </h4>
         <h4>Yearly Income: {app.yearly_income}</h4>
         <h4>Current Residence Address: {app.current_residence_address}</h4>
         <h4>Current Residence Phone: {app.current_residence_phone}</h4>
-        {/* <button>Update</button> */}
-        {/* when user hits update render <UpdateApplictionForm */}
-        <button onClick={() => handleApplicationDelete(id, apartment_id)}>Delete</button>
-        <button>Update</button>
 
-        <UpdateApplicationForm application={app} handleUpdatingUserApplication={handleUpdatingUserApplication} />
+        <button onClick={() => handleApplicationDelete(id, apartment_id)}>Delete</button>
+        <button onClick={() => setIsClicked(true)}>Update</button>
+        <div style={{display: isClicked ? null : "none"}}>
+            <UpdateApplicationForm app={app} handleUpdatingUserApplication={handleUpdatingUserApplication} setIsClicked={setIsClicked}/>
+        </div>
     </div>
     )
 }
