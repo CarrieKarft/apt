f9e661c271fd46cc23a887c9ec567cdb18026cd8
Carrie Kraft <carriekarft@gmail.com>
2023-12-21 19:35:54 -0800

    removed unneeded comments

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -1,5 +1,3 @@
 class ApplicationSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :apartment_id, :applicant_name, :current_residence_phone, :current_residence_address, :yearly_income, :applicant_email
-  # belongs_to :user
-  # belongs_to :
 end
