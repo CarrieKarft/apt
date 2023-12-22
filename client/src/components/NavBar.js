7940259bc9d4871a4e15577b308a63fd1ca0517a
Carrie Kraft <carriekarft@gmail.com>
2023-12-21 19:58:05 -0800

    removed unneeded comments

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,10 +2,7 @@ import {NavLink} from 'react-router-dom';
 
 function NavBar({ onHandleLogout }) {
     return (
-        // might get rid of view applicaitons
-        // all of these will become links with reactRouterDom
-        // search will take you to AppartmetsListingPage
-        // Logout will trigger fetch to sessions#delete
+
         <div>
             <button onClick={(e) => onHandleLogout(e.target.value)}>Logout</button>
             <NavLink to={'/appartment-listings'}><button>Search For Appartments</button></NavLink>
