3132940d3fdd3a734b8f31673d86463b11886314
Carrie Kraft <carriekarft@gmail.com>
2023-12-21 19:38:44 -0800

    removed Error component, added alerts to handle error messages, fixed state management to update curretUser.unique apartments for applications create and delete methods

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import {Routes, Route} from "react-router-dom";
-import { useState, useContext } from 'react';
+import './index.css';
+import {Routes, Route, useNavigate} from "react-router-dom";
+import { useContext } from 'react';
 import AppartmentsListingPage from './components/ApartmentsListingPage';
 import SignupPage from './components/SignupPage';
 import LoggedInUserPage from './components/LoggedInUserPage';
 import NewApplicationForm from './components/NewApplicationForm';
 import CompletedAppliction from './components/CompletedApplication';
-import UpdateApplicationForm from './components/UpdateApplicaitonForm';
+import Header from './components/Header';
 import { ApartmentContext } from './context/ApartmentContext';
 import {UserContext} from '../src/context/UserContext'
 
@@ -14,15 +15,16 @@ import {UserContext} from '../src/context/UserContext'
 function App() {
   const {currentUser, setCurrentUser} = useContext(UserContext);
   const {apartments, setApartments} = useContext(ApartmentContext);
-  const [application, setApplication] = useState();
 
-  if(!currentUser) return <h2>Loading...</h2>
+  const navigate = useNavigate();
+
+  if(!currentUser) return <SignupPage handleSignupUser={handleSignupUser} />
 
   const {applications} = currentUser
+  console.log(currentUser.apartments)
 
   function handleSignupUser(newUserObj) {
-    console.log(newUserObj)
-    fetch('/users', {
+    fetch('/signup', {
         method: 'POST',
         headers: {
             "Content-Type": "application/json",
@@ -30,22 +32,15 @@ function App() {
         body: JSON.stringify(newUserObj)
     }).then(r => {
       if(r.ok) {
-          r.json().then(newUserData => handleUpdatingUsers(newUserData))
+          r.json().then(newUserData => setCurrentUser(newUserData))
       } else {
-          r.json().then(errorData => console.log(errorData.errors))
+          r.json().then(errorData => alert(errorData.errors))
       }
   })
-  }
 
-  function handleUpdatingUsers(newUserData) {
-    // dont want to set user with this info since it contains password, need to use history.push to direct user to logged in user page or have this fetch '/me'
-    console.log(newUserData)
   }
 
   function handleUpdatingUserApplication(updatedApplicationObj) {
-    console.log("second update obj", updatedApplicationObj)
-    // const {id} = updatedApplicationObj
-    console.log("id", updatedApplicationObj.id)
     fetch(`/applications/${updatedApplicationObj.id}`, {
       method: "PATCH",
       headers: {
@@ -54,37 +49,34 @@ function App() {
       body: JSON.stringify(updatedApplicationObj),
     }).then(r => {
       if(r.ok) {
-          r.json().then(updatedAppData => handleUpdatingApplicationInUserState(updatedAppData))
+          r.json().then(updatedAppData => handleUpdatingApplicationInState(updatedAppData))
       } else {
-          r.json().then(errorData => console.log(errorData.errors))
+          r.json().then(errorData => alert(errorData.errors))
       }
     })
 
+
   }
 
-  function handleUpdatingApplicationInUserState(updatedAppData){
-    console.log(updatedAppData)
-    console.log("before state change", currentUser)
+  function handleUpdatingApplicationInState(updatedAppData){
+    // console.log("before state change", currentUser)
     const filteringOutOldApplication = applications.map(app => app.id === updatedAppData.id ? updatedAppData : app)
-    console.log(filteringOutOldApplication)
     const updatedCurrentUser = {...currentUser, applications: filteringOutOldApplication}
-
-
     setCurrentUser(updatedCurrentUser);
 
-    const findingApartmentRelatedToApp = apartments.find(apt => apt.id == updatedAppData.apartment_id)
+    const findingApartmentRelatedToApp = apartments.find(apt => apt.id === updatedAppData.apartment_id)
     const mapingAptsAppsToUpdate = findingApartmentRelatedToApp.applications.map(app => app.id === updatedAppData.id ? updatedAppData : app)
     const updatingAptWithNewAppsArray = {...findingApartmentRelatedToApp, applications: mapingAptsAppsToUpdate}
-    console.log("3", updatingAptWithNewAppsArray)
     const mappingAptsArrayToAddUpdatedApt = apartments.map(apt => updatingAptWithNewAppsArray.id === apt.id ? updatingAptWithNewAppsArray : apt)
-    console.log(mappingAptsArrayToAddUpdatedApt)
     setApartments(mappingAptsArrayToAddUpdatedApt)
 
+
+
   }
-  console.log("after state change", currentUser)
+  // console.log("after state change", currentUser)
+  // console.log("after state change", apartments)
 
   function handleCreatingNewApplicaiton(newApplicaitonObj) {
-    // console.log(newApplicaitonObj)
     fetch('/applications', {
             method: "POST",
             headers: {
@@ -93,56 +85,92 @@ function App() {
             body: JSON.stringify(newApplicaitonObj),
           }).then(r => {
             if(r.ok) {
-                r.json().then(newAppData => handleAddingApplicationToUserState(newAppData))
+                r.json().then(newAppData => handleAddingApplicationToState(newAppData))
             } else {
-                r.json().then(errorData => console.log(errorData.errors))
+                r.json().then(errorData => alert(errorData.errors))
             }
         })
   }
 
-  function handleAddingApplicationToUserState(newAppData){
-
+  function handleAddingApplicationToState(newAppData){
+    console.log("before", apartments)
+    console.log("before", currentUser)
     const newApplications = [...currentUser.applications, newAppData]
-    const updatedCurrentUser = {...currentUser, applications: newApplications}
-    setCurrentUser(updatedCurrentUser);
 
-    const findingApartmentRelatedToApp = apartments.find(apt => apt.id == newAppData.apartment_id)
+    const findingApartmentRelatedToApp = apartments.find(apt => apt.id === newAppData.apartment_id)
     const addingAppToApartmentApplications = [...findingApartmentRelatedToApp.applications, newAppData]
     const updatingApartmentRelatedToApp = {...findingApartmentRelatedToApp, applications: addingAppToApartmentApplications}
-    const addingUpdatedAptToApartments = apartments.map(apt => apt.id == newAppData.apartment_id ? updatingApartmentRelatedToApp : apt) 
+    const addingUpdatedAptToApartments = apartments.map(apt => apt.id === newAppData.apartment_id ? updatingApartmentRelatedToApp : apt) 
     setApartments(addingUpdatedAptToApartments)
-  }
 
+    const filteringAptOutOfUniqueApartments = currentUser.unique_apartments.filter(apt => apt.id !== findingApartmentRelatedToApp.id)
+
+    const creatingNewUniqueApartment = {
+      address: findingApartmentRelatedToApp.address,
+      available_to_rent: findingApartmentRelatedToApp.available_to_rent,
+      id: findingApartmentRelatedToApp.id,
+      monthly_rent: findingApartmentRelatedToApp.monthly_rent,
+      name: findingApartmentRelatedToApp.name,
+      number_of_rooms: findingApartmentRelatedToApp.number_of_rooms,
+      parking_available: findingApartmentRelatedToApp.parking_available,
+      pets_allowed: findingApartmentRelatedToApp.pets_allowed,
+      phone: findingApartmentRelatedToApp.phone,
+    }
+
+    const addingAptBackInToUniqueApartments = [...filteringAptOutOfUniqueApartments, creatingNewUniqueApartment]
+    const addingNewUniqueApartmentArrayToCurrentUser = {...currentUser, applications: newApplications, unique_apartments: addingAptBackInToUniqueApartments}
+    setCurrentUser(addingNewUniqueApartmentArrayToCurrentUser)
+
+
+    navigate('/user-profile')
+  }
+  console.log("after", apartments)
+  console.log("after", currentUser)
 
   function handleApplicationDelete(id, apartment_id){
     fetch(`/applications/${id}`, {
             method: "DELETE"
         })
-        .then(r => r.json())
-        .then(() => filteringDetetedApplicationFromState(id, apartment_id))
+        .then(r => {
+          if(r.ok) {
+            r.json().then(() => filteringDetetedApplicationFromState(id, apartment_id))
+        } else {
+            r.json().then(errorData => alert(errorData.errors))
+        }
+    })
   }
 
   function filteringDetetedApplicationFromState(id, apartment_id) {
-    const filteringOutDeletedApplication = applications.filter(app => app.id !== id)
-    const updatingCurrentUser = {...currentUser, applications: [filteringOutDeletedApplication]}
-    setCurrentUser(updatingCurrentUser)
-
+    // console.log("before state change", currentUser)
+    // console.log("before state change", apartments)
     const findingApartmentRelatedToApp = apartments.find(apt => apt.id === apartment_id)
     const filteringOutApplicationForApt = findingApartmentRelatedToApp.applications.filter(app => app.id !== id)
     const updatingAptRelatedToApp = {...findingApartmentRelatedToApp, applications: filteringOutApplicationForApt}
     const addingUpdatedAptBackToApartments = apartments.map(apt => apt.id === updatingAptRelatedToApp.id ? updatingAptRelatedToApp : apt)
     setApartments(addingUpdatedAptBackToApartments)
+
+    const filteringOutDeletedApplication = applications.filter(app => app.id !== id)
+
+    const appsRelatedToAptAndCurrentUser = filteringOutDeletedApplication.some(app => app.apartment_id === findingApartmentRelatedToApp.id)
+
+    if (!appsRelatedToAptAndCurrentUser) {
+      const filteringOutUniqueApt = currentUser.unique_apartments.filter(apt => findingApartmentRelatedToApp.id !== apt.id)
+      const updatingCurrentUserWithApts = {...currentUser, applications: filteringOutDeletedApplication, unique_apartments: filteringOutUniqueApt}
+      setCurrentUser(updatingCurrentUserWithApts)
+    } else {
+      const updatingCurrentUser = {...currentUser, applications: filteringOutDeletedApplication}
+      setCurrentUser(updatingCurrentUser)
+    }
   }
 
 
   return (
-    <div>
+    <div className="App">
+      <Header />
         <Routes>
           <Route path='/appartment-listings' element={<AppartmentsListingPage />} />
-          <Route path='/appartment/:id/application' element={<CompletedAppliction handleUpdatingUserApplication={handleUpdatingUserApplication} application={application} setApplication={setApplication} handleApplicationDelete={handleApplicationDelete}/>} />
-          <Route path='/applications/:id' element={ <UpdateApplicationForm handleUpdatingUserApplication={handleUpdatingUserApplication} />}></Route>
+          <Route path='/appartment/:id/application' element={<CompletedAppliction handleUpdatingUserApplication={handleUpdatingUserApplication} handleApplicationDelete={handleApplicationDelete}/>} />
           <Route path='/apartment/:id/new-application' element={<NewApplicationForm onHandleCreatingNewApplicaiton={handleCreatingNewApplicaiton}/>} />
-          <Route path='/signup-page' element={<SignupPage handleSignupUser={handleSignupUser}/>} />
           <Route path='/user-profile' element={<LoggedInUserPage />} />
         </Routes>
     </div>
