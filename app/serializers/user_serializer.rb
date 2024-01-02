2b3d73d00a0eeb9dc2c241ef174f1b68514b87dc
Carrie Kraft <carriekarft@gmail.com>
2024-01-01 16:44:18 -0800

    remove comments and has_many macros since user.applications isnt used in application anywhere and apartments is changed to unique_apartments

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,19 +1,13 @@
 class UserSerializer < ActiveModel::Serializer
-  # took out password_digest and added an additional has_many
   attributes :id, :name, :username, :phone, :location, :first_name, :unique_apartments
-  # need to display appartments info not applications
-  has_many :applications
-  has_many :apartments
 
   def unique_apartments
     apartments = object.apartments
-    # byebug
     apartments.uniq {|apt| apt.id}
   end
 
   def first_name
     name = object.name.split(" ")
     name.first
-    # byebug
   end
 end
