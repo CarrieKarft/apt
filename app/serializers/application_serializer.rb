868cd61aea4f0655834b0eb73f7893071b579d6d
Carrie Kraft <carriekarft@gmail.com>
2024-01-01 16:42:47 -0800

    removed belongs_to macro since applications doesnt need to have nested associated data

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -1,3 +1,4 @@
 class ApplicationSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :apartment_id, :applicant_name, :current_residence_phone, :current_residence_address, :yearly_income, :applicant_email
+
 end
