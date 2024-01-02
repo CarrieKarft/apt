824a18b033e388e767a0236baca99d6a9e90108d
Carrie Kraft <carriekarft@gmail.com>
2024-01-01 16:38:21 -0800

    removed comments

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,11 +1,6 @@
 class ApplicationsController < ApplicationController
-    # need to add some application data to test these methods
 
     def create
-        # needs to render authorize error if user not logged in
-        # needs to render invlide exception if info is invalid
-
-
         user = find_user
         application = user.applications.create!(applicaition_params)
         render json: application, status: :created
@@ -13,11 +8,6 @@ class ApplicationsController < ApplicationController
     end
 
     def update
-        # needs to render authorize error if user not logged in
-        # needs to render invlide exception if info is invalid
-        # needs to raise not found exception if application not found
-
-
         user = find_user
         application = user.applications.find(params[:id])
         updated_application = application.update!(applicaition_params)
@@ -25,10 +15,6 @@ class ApplicationsController < ApplicationController
     end
 
     def destroy
-        # needs to render authorize error if user not logged in
-        # needs to raise not found exception if application not found
-
-
         user = find_user
         application = user.applications.find(params[:id])
         application.delete
