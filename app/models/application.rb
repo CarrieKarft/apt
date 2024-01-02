017f1c10729f69aeab1d7880418c4a3c21063156
Carrie Kraft <carriekarft@gmail.com>
2024-01-01 16:40:26 -0800

    removed comments

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,8 +1,7 @@
 class Application < ApplicationRecord
     belongs_to :user
     belongs_to :apartment
-    # validates :appartment_id, presence: :true
-    # clean up validations
+
     validates :applicant_name, presence: :true
     validates :applicant_email, presence: true
     validates :current_residence_phone, presence: :true, length: {minimum: 10}
