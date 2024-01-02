b5a8b0f6f4862eecba22dc5408d93decfd23e3c5
Carrie Kraft <carriekarft@gmail.com>
2024-01-01 16:39:17 -0800

    removed comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,7 @@
 class SessionsController < ApplicationController
   skip_before_action :authorize, only: [:create]
-  # logs user in
+
   def create
-    # works
     user = User.find_by(username: params[:username])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
@@ -11,9 +10,8 @@ class SessionsController < ApplicationController
       render json: {error: "Wrong username or password"}, status: 422
     end
   end
-# logs user out by deleting the user_id from the session hash
+
   def destroy
-    # works
     session.delete :user_id
     head :no_content
   end
